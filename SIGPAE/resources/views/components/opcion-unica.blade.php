fcbb4fa76bc2f8362712cb6c3cc51065a96e7855
Garcia Schmidt Barbara <barbygs1900@gmail.com>
2025-10-12 20:52:27 -0300

    se crea la base de la vista plan de accion. Faltan componentes

--- SIGPAE/resources/views/components/opcion-unica.blade.php
+++ SIGPAE/resources/views/components/opcion-unica.blade.php
@@ -1,10 +1,11 @@
-<div class="checkbox-group">
+<div class="radio-group">
     @foreach ($items as $item)
-        <label class="checkbox-item">
+        <label class="radio-item">
             <input type="radio"
                    name="{{ $name }}"
                    value="{{ $item }}"
                    {{ old($name) === $item ? 'checked' : '' }}>
+            <span class="custom-radio"></span>
             {{ $item }}
         </label>
     @endforeach
--- SIGPAE/resources/views/planDeAccion/principal.blade.php
+++ SIGPAE/resources/views/planDeAccion/principal.blade.php
@@ -3,15 +3,42 @@
 @section('encabezado', 'Planes de acción')
 
 @section('contenido')
-    <!-- contenido específico -->
-      <x-boton-aceptar>Guardar</x-boton-aceptar>
-      <x-boton-aceptar class="bg-danger">Eliminar mi cuenta</x-boton-aceptar>
-
-      @php
-        $intervenciones = ['Psicopedagógica', 'Social', 'Académica', 'Familiar'];
-      @endphp
-
-        <x-checkboxes :items="$intervenciones" name="tipo_intervencion" />
-        <p>Seleccione una única intervención:</p>
-        <x-opcion-unica :items="$intervenciones" name="tipo_intervencion_unica" />
+    <div class="fila-componentes">
+      <x-boton-aceptar>Crear</x-boton-aceptar>
+      <boton>Tipo</boton>
+      <boton>Estado</boton>
+      <boton>Curso</boton>
+      <boton>Buscar</boton>
+    </div>
+    <p>Aquí va la tabla de planes. REDEFINIR CON COMPONENTES BLADE</p>
+    <table>
+        <thead>
+            <tr>
+                <th>Estado</th>
+                <th>Tipo</th>
+                <th>Destinatarios</th>
+                <th>Responsables</th>
+                <th>Acciones</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- Aquí van las filas de la tabla -->
+            <tr>
+                <td>Abierto</td>
+                <td>Institucional</td>
+                <td>Escuela n°41</td>
+                <td>PS. Juan Flores</td>
+                <td><boton>Ver</boton> <boton>Editar</boton></td>
+            </tr>
+            <tr>
+                <td>Abierto</td>
+                <td>Individual</td>
+                <td>Maria Pepe</td>
+                <td>AS. Lucas Diaz</td>
+                <td><boton>Ver</boton> <boton>Editar</boton></td>
+            </tr>
+            <!-- Más filas según sea necesario -->
+        </tbody>
+    </table>
+    <boton>Volver</boton>
 @endsection
