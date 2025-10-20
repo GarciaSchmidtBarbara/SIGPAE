93f80aa4c56c116c0da18201a9d22318dfb01797
ibelinday <i_belinda_y@hotmail.com>
2025-10-20 19:48:37 -0300

    Descripción breve del cambio

---
+++ SIGPAE/resources/views/components/boton-buscar.blade.php
@@ -0,0 +1,11 @@
+<div class="relative w-80">
+    <input
+        type="text"
+        name="search"
+        placeholder="Buscar"
+        class="w-full pl-4 pr-10 py-2 text-2xl text-gray-400 border border-gray-400 rounded-xl focus:outline-none focus:ring-1 focus:ring-gray-400"
+    >
+    <div class="absolute inset-y-0 right-3 flex items-center pointer-events-none text-2xl">
+        🔍
+    </div>
+</div>
