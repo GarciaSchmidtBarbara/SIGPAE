96f846174d86db51708896e316bd966eb0f4fb52
Lucas Vela <lucas.tito1@hotmail.com>
2025-10-31 21:16:36 -0300

    Arreglados algunos modelos 	deleted:    SIGPAE/app/Models/Evaluacion.php

--- SIGPAE/app/Models/EvaluacionDeIntervencionEspontanea.php
+++ SIGPAE/app/Models/EvaluacionDeIntervencionEspontanea.php
@@ -6,8 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class EvaluacionDeIntervencionEspontanea extends Model
 {
-    // nombre personalizado de la tabla asociada al modelo
-    protected $table = 'evaluaciones_de_intervencion_espontanea';
-    // clave primaria personalizada
-    protected $primaryKey = 'id_evaluaciones_de_intervencion_espontanea';
+    protected $table = 'evaluaciones_intervenciones_espontaneas';
+    protected $primaryKey = 'id_evaluacion_intervencion_espontanea';
+    protected $fillable = [
+        'fecha_evaluacion',
+        'observaciones',
+        'criterios',
+        'conclusiones',
+    ];
+    
+    public function intervencionEspontanea()
+    {
+        return $this->hasmany(IntervencionEspontanea::class, 'fk_id_intervencion_espontanea', 'id_intervencion_espontanea');
+    }
 }
