04f345f0d13c6cd55a0641fd9db10c48b0650981
Garcia Schmidt Barbara <barbygs1900@gmail.com>
2025-11-30 20:39:56 -0300

    Se eliminan metodos: obtenerTodos, filtrar (repetido), buscar y listar. Se cambia nombres de metodos de "obtener" a "buscar" Se redefine metodo "datosParaFormulario" para que consulte a otros service sobre datos que se requieren

--- SIGPAE/app/Repositories/Interfaces/PlanDeAccionRepositoryInterface.php
+++ SIGPAE/app/Repositories/Interfaces/PlanDeAccionRepositoryInterface.php
@@ -10,12 +10,11 @@ interface PlanDeAccionRepositoryInterface
     public function crear(array $data): PlanDeAccion;
     public function eliminar(int $id): bool;
     public function cambiarActivo(int $id): bool;
-    public function filtrar(array $filtros): \Illuminate\Database\Eloquent\Builder;
     public function buscarPorIdPersona(int $idPersona): ?PlanDeAccion;
-    public function obtenerPlanesFiltrados(Request $request): Collection;
-    public function obtenerAulasParaFiltro(): Collection;
-    public function obtenerPorId(int $id): ?PlanDeAccion;
-    public function obtenerTodos(): Collection;
+    public function filtrar(Request $request): Collection;
+    public function obtenerAulas(): Collection;
+    public function obtenerModelosAulas():Collection; //devuelve modelos Eloquent
+    public function buscarPorId(int $id): ?PlanDeAccion;
     public function buscarPorIdConRelaciones(int $id);
     
 }
