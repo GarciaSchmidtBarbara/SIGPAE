a4829ad4753a17d68147a8a2439b13dacdcc05ef
Belinda <i_belinda_y@hotmail.com>
2025-12-01 08:38:47 -0300

    Controller para planilla

---
+++ SIGPAE/app/Http/Controllers/PlanillaController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Planilla;
+
+class PlanillaController extends Controller
+{
+    // 1. Mostrar el formulario
+    public function crearActaIndisciplinario()
+    {
+        // Datos falsos para que la tabla no se vea vacía al inicio
+        $datos_falsos = [
+            ['cargo' => 'EI', 'nombre' =>'Psp. Sandra Yamzon','asistio' => false],
+            ['cargo' => 'EI', 'nombre' =>'Ps. Claudio Casele','asistio' => true],
+            ['cargo' => 'EI', 'nombre' =>'Ps. Mercedez Carreño','asistio' => true],
+            ['cargo' => 'EI', 'nombre' =>'As. Hugo Gonzales','asistio' => true],
+            ['cargo' => 'EI', 'nombre' =>'Psp. Lucía Iglesia','asistio' => true],
+            ['cargo' => 'EI', 'nombre' =>'Ae. Pablo Brizuela','asistio' => true],
+            ['cargo' => 'EI', 'nombre' =>'Pga. Mayra Brito','asistio' => true],
+            ['cargo' => 'Director', 'nombre' => '', 'asistio' => false],
+            ['cargo' => 'Vicedirector', 'nombre' => '', 'asistio' => false],
+        ];
+
+        return view('planillas.acta-equipo-indisciplinario', ['personal' => $datos_falsos]);
+    }
+
+    // 2. Guardar el formulario
+    public function guardarActaIndisciplinario(Request $request)
+    {
+        $participantes = json_decode($request->participantes_json, true);
+
+        // Preparamos el contenido
+        $contenido = [
+            'grado'           => $request->grado,
+            'fecha'           => $request->fecha,
+            'hora'            => $request->hora,
+            'participantes'   => $participantes,
+            'temario'         => $request->temario,
+            'acuerdo'         => $request->acuerdo,
+            'observaciones'   => $request->observaciones,
+            'proxima_reunion' => $request->proxima_reunion,
+        ];
+
+        // Guardamos en BD
+        Planilla::create([
+            'tipo_planilla'   => 'ACTA REUNIÓN EQUIPO INTERDISCIPLINARIO- equipo directivo',
+            'nombre_planilla' => 'Acta EI Directivo - ' . $request->fecha,
+            'anio'            => date('Y'),
+            'datos_planilla'  => $contenido
+        ]);
+
+        return redirect()
+            ->route('planillas.acta-equipo-indisciplinario.create')
+            ->with('success', '¡Acta guardada correctamente!');
+    }
+}
