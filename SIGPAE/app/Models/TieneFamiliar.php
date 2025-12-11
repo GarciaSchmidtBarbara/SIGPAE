1b4ae7a653781c5837d3400de6cd61e10c3c2d4c
Eliezer Sardinas <sardinaseliezer@gmail.com>
2025-12-11 13:17:23 -0300

    creo un modelo porque necesito ejercer reglas de negocio sobre los datos de la tabla pivot tiene_familiar

---
+++ SIGPAE/app/Models/TieneFamiliar.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Relations\Pivot;
+use App\Enums\Parentesco;
+
+class TieneFamiliar extends Pivot
+{
+    protected $table = 'tiene_familiar';
+
+    public $incrementing = true;
+
+    protected $fillable = [
+        'fk_id_alumno',
+        'fk_id_familiar',
+        'parentesco',
+        'observaciones',
+        'activa',
+        'otro_parentesco'
+    ];
+
+    protected $casts = [
+        'parentesco' => Parentesco::class,
+        'activa'     => 'boolean',
+    ];
+}
