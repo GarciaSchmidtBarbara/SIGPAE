03e9561c8fa07087547c4b6aa01b36da0cd162fe
Garcia Schmidt Barbara <barbygs1900@gmail.com>
2026-02-27 19:10:54 -0300

    se editan algunos componentes para hacerlos responsive

--- resources/views/components/opcion-unica.blade.php
+++ resources/views/components/opcion-unica.blade.php
@@ -5,18 +5,21 @@
     'seleccion' => null,
 ])
 
-<div class="custom-radio-group {{ $layout === 'horizontal' ? 'flex gap-4 flex-wrap items-center' : 'space-y-2' }}">
+<div class="{{ $layout === 'horizontal' 
+    ? 'flex gap-4 flex-wrap items-center' 
+    : 'space-y-2' }}">
+
     @foreach ($items as $item)
-        <label class="flex items-center gap-2 cursor-pointer {{ $layout === 'horizontal' ? 'min-w-[100px]' : '' }}">
-            <input type="radio"
-                   class="custom-radio"
+        <label class="flex items-center gap-2 cursor-pointer">
+             <input type="radio"
                    name="{{ $name }}"
                    value="{{ $item }}"
+                   class="w-4 h-4 text-indigo-600 border-gray-300 focus:ring-indigo-500"
                    {{ old($name, $seleccion) == $item ? 'checked' : '' }}
-                   
-                   {{ $attributes->whereStartsWith('x-model') }} 
-                   >
-            <span class="text-gray-700">{{ $item }}</span>
+                   {{ $attributes->whereStartsWith('x-model') }}
+            >
+            <span class="text-sm text-gray-700">{{ $item }}</span>
         </label>
+
     @endforeach
 </div>
